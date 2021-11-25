d6075a6e9c1de73047dec90a82df905f44109276
tcrain96 <tristan.crain@outlook.com>
2021-11-25 15:14:52 -0500

    added length prompt to password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,48 @@
 // Assignment code here
+var passwordInfo = {
+  length:"0",
+  setLength(){
 
+    //loop until input is either canceled or correctly inputed
+    while(true){
+      
+      var tempLength = window.prompt("Please input the length of password you wish to generate");
+
+      //if user pressed cancel
+      if(tempLength === null){
+        //set length to null
+        this.length = null;
+        return;
+      }
+
+      //else if the user inputed a number, and it was between 8 and 128
+      else if((!isNaN(parseInt(tempLength))) && parseInt(tempLength) >= 8 && parseInt(tempLength) <=128){
+        //set length of password to length inputed
+        this.length = tempLength;
+      }
+
+      //tell user their input was invalid, and ask if they wish to try again. If yes, return to top, if cancel, set length to null.
+      else{
+        if(!window.confirm("Please input a number between 1 and 128")){
+          this.length = null;
+          return;
+        }
+      }
+    }
+  }
+}
+
+function generatePassword(){
+  var password;
+
+  passwordInfo.setLength();
+
+  if(passwordInfo.length === null){
+    password = "Password generation has been canceled";
+  }
+
+  return password; 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
